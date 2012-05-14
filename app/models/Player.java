e8ae73ebb530fd3539e881937fcc51e36425b56e
Mary Craig <marycraig90@gmail.com>
2012-05-13 23:07:54 -0300

    Logica de juego Bye bye Board (lo borre!)

--- app/models/Player.java
+++ app/models/Player.java
@@ -1,5 +1,6 @@
 package models;
 
+
 import org.codehaus.jackson.JsonNode;
 import play.mvc.WebSocket;
 
@@ -22,6 +23,8 @@ public class Player {
     Player enemy=null;
     List<Ship> ships;
     List<String> previousShots;
+    /*Esto por ahi hay q cambiarlo */
+    ShootResults result;
 
     public Player(String username, WebSocket.Out<JsonNode> channel,boolean myTurn) {
         this.username = username;
@@ -30,6 +33,7 @@ public class Player {
         board=new Board();
         ships = createShips();
         previousShots = new ArrayList<String>();
+        defaultStrategy();
     }
 
     private List<Ship> createShips() {
@@ -57,15 +61,13 @@ public class Player {
     }
    public String attack(String tile){
        previousShots.add(tile);
-       System.out.println(previousShots.get(previousShots.size()-1));
-       System.out.println(previousShots.size());
        return enemy.shoot(tile);
    }
 
     public String enemyResult(String tile){
-        switch(board.shoot(tile)){
-                  case ALREADY_SHOT:return "The dogs shot the same spot again!";
-                  case HIT: return "Arghh! We got hit!";
+        switch(result){
+                  case ALREADY_SHOT:return "The grogged monkeys shot the same spot again!";
+                  case HIT: return "Arrrgh! We got hit!";
                   case SUNK: return "Abandon ship!!!";
                   case WATER:return "Hurrah!! They missed!";
                   default: return "Something went wrong in Player.shoot!";
@@ -73,7 +75,8 @@ public class Player {
     }
 
     public String shoot (String tile){
-      switch(board.shoot(tile)){
+        result = checkResult(tile);
+        switch(result){
           case ALREADY_SHOT:return "Captain, we already shot "+tile;
           case HIT: return "Bull's eye Captain!";
           case SUNK: return "We sent them straight to Hell my Captain!";
@@ -82,5 +85,75 @@ public class Player {
       }
     }
 
+    //Todo: Mejorar la parte de SUNK, que saque al barco de la lista o algo
+    public ShootResults checkResult(String tile){
+        for(String shot:previousShots){
+            if(previousShots.equals(shot)){
+                return ShootResults.ALREADY_SHOT;
+            }
+        }
 
+        for(Ship ship:ships){
+            for(int j=0;j<ship.getPositions().length;j++){
+                if(ship.getPositions()[j].equals(tile)){
+                    if(ship.getHits()==ship.getSize()){
+                        return ShootResults.SUNK;
+                    }else{
+                        ship.setHits(ship.getHits()+1);
+                        return ShootResults.HIT;
+                    }
+                }
+            }
+        }
+
+        return ShootResults.WATER;
+
+    }
+
+   /*La lista tiene que tener los arrays en orden, del barco mas chico al barco mas grande*/
+   public void addShipPositions(List<String[]> shipPositions){
+       for(int i=0; i<shipPositions.size();i++){
+            ships.get(i).setPositions(shipPositions.get(i));
+
+        }
+
+   }
+
+    public void defaultStrategy(){
+        List<String[]>strategy = new ArrayList<String[]>(5);
+
+        String[] s1 = new String[2];
+        s1[0]="A1";
+        s1[1]="A2";
+        strategy.add(s1);
+
+        String[] s2 = new String[3];
+        s2[0]="B3";
+        s2[1]="C3";
+        s2[2]="D3";
+        strategy.add(s2);
+
+        String[] s3 = new String[3];
+        s3[0]="G6";
+        s3[1]="G7";
+        s3[2]="G8";
+        strategy.add(s3);
+
+        String[] s4 = new String[4];
+        s4[0]="A5";
+        s4[1]="B5";
+        s4[2]="C5";
+        s4[3]="D5";
+         strategy.add(s4);
+
+        String[] s5 = new String[5];
+        s5[0]="J5";
+        s5[1]="J6";
+        s5[2]="J7";
+        s5[3]="J8";
+        s5[4]="J9";
+        strategy.add(s5);
+
+        addShipPositions(strategy);
+    }
 }
--- app/models/Ship.java
+++ app/models/Ship.java
@@ -11,14 +11,46 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class Ship {
+
     private String[] positions;
+
+
     private int size;
     private String name;
 
+
+
+    private int hits;
+
     public Ship(int size,String name){
-          this.size=size;
+         this.size=size;
         this.name=name;
-          positions = new String[size];
+         positions = new String[size];
+        hits = 0;
     }
 
+    public String[] getPositions() {
+         return positions;
+     }
+
+     public void setPositions(String[] positionArray) {
+         positions=positionArray;
+     }
+
+
+    public int getHits() {
+        return hits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setHits(int hits) {
+            this.hits = hits;
+        }
 }
